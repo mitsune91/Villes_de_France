a4edba8e5e815a45ec3bb79b23651225691d5ec4
unknown <tho.giotti@gmail.com>
2019-11-08 16:22:40 +0100

    Update db with no cache and add file to repository

---
+++ web/sites/default/files/js/js_TMpDBrVEKIoIoFBDBZ13CkyhuNTEApIRLU16BgDMwU4.js
@@ -0,0 +1,228 @@
+/**
+ * @file
+ * Handle the common map.
+ */
+
+/**
+ * @name CommonMapUpdateSettings
+ * @property {String} enable
+ * @property {String} hide_form
+ * @property {number} views_refresh_delay
+ * @property {String} update_view_id
+ * @property {String} update_view_display_id
+ * @property {String} boundary_filter
+ * @property {String} parameter_identifier
+ */
+
+/**
+ * @name CommonMapSettings
+ * @property {Object} settings
+ * @property {CommonMapUpdateSettings} dynamic_map
+ * @property {Boolean} markerScrollToResult
+ */
+
+/**
+ * @property {CommonMapSettings[]} drupalSettings.geolocation.commonMap
+ */
+
+(function ($, window, Drupal, drupalSettings) {
+  'use strict';
+
+  /**
+   * Attach common map style functionality.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches common map style functionality to relevant elements.
+   */
+  Drupal.behaviors.geolocationCommonMap = {
+    /**
+     * @param {GeolocationSettings} drupalSettings.geolocation
+     */
+    attach: function (context, drupalSettings) {
+      if (typeof drupalSettings.geolocation === 'undefined') {
+        return;
+      }
+
+      $.each(
+        drupalSettings.geolocation.commonMap,
+
+        /**
+         * @param {String} mapId - ID of current map
+         * @param {CommonMapSettings} commonMapSettings - settings for current map
+         */
+        function (mapId, commonMapSettings) {
+
+          var map = Drupal.geolocation.getMapById(mapId);
+
+          if (!map) {
+            return;
+          }
+
+          /*
+           * Hide form if requested.
+           */
+          if (
+            typeof commonMapSettings.dynamic_map !== 'undefined'
+            && commonMapSettings.dynamic_map.enable
+            && commonMapSettings.dynamic_map.hide_form
+            && typeof commonMapSettings.dynamic_map.parameter_identifier !== 'undefined'
+          ) {
+            var exposedForm = $('form#views-exposed-form-' + commonMapSettings.dynamic_map.update_view_id.replace(/_/g, '-') + '-' + commonMapSettings.dynamic_map.update_view_display_id.replace(/_/g, '-'));
+
+            if (exposedForm.length === 1) {
+              exposedForm.find('input[name^="' + commonMapSettings.dynamic_map.parameter_identifier + '"]').each(function (index, item) {
+                $(item).parent().hide();
+              });
+
+              // Hide entire form if it's empty now, except form-submit.
+              if (exposedForm.find('input:visible:not(.form-submit), select:visible').length === 0) {
+                exposedForm.hide();
+              }
+            }
+          }
+        }
+      );
+    },
+    detach: function (context, drupalSettings) {}
+  };
+
+  Drupal.geolocation.commonMap = Drupal.geolocation.commonMap || {};
+
+  Drupal.geolocation.commonMap.dynamicMapViewsAjaxSettings = function (commonMapSettings) {
+    // Make sure to load current form DOM element, which will change after every AJAX operation.
+    var view = $('.view-id-' + commonMapSettings.dynamic_map.update_view_id + '.view-display-id-' + commonMapSettings.dynamic_map.update_view_display_id);
+
+    if (typeof commonMapSettings.dynamic_map.boundary_filter === 'undefined') {
+      return;
+    }
+
+    // Extract the view DOM ID from the view classes.
+    var matches = /(js-view-dom-id-\w+)/.exec(view.attr('class').toString());
+    var currentViewId = matches[1].replace('js-view-dom-id-', 'views_dom_id:');
+
+    var viewInstance = Drupal.views.instances[currentViewId];
+    var ajaxSettings = $.extend(true, {}, viewInstance.element_settings);
+    ajaxSettings.progress.type = 'none';
+
+    var exposedForm = $('form#views-exposed-form-' + commonMapSettings.dynamic_map.update_view_id.replace(/_/g, '-') + '-' + commonMapSettings.dynamic_map.update_view_display_id.replace(/_/g, '-'));
+    if (exposedForm.length) {
+      // Add form values.
+      jQuery.each(exposedForm.serializeArray(), function (index, field) {
+        var add = {};
+        add[field.name] = field.value;
+        ajaxSettings.submit = $.extend(ajaxSettings.submit, add);
+      });
+    }
+
+    // Trigger geolocation bounds specific behavior.
+    ajaxSettings.submit = $.extend(ajaxSettings.submit, {geolocation_common_map_dynamic_view: true});
+
+    return ajaxSettings;
+  };
+
+})(jQuery, window, Drupal, drupalSettings);
+;
+/**
+ * @file
+ * Common Map Leaflet.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Dynamic map handling aka "AirBnB mode".
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches common map style functionality to relevant elements.
+   */
+  Drupal.behaviors.geolocationCommonMapLeaflet = {
+    /**
+     * @param {GeolocationSettings} drupalSettings.geolocation
+     */
+    attach: function (context, drupalSettings) {
+      $.each(
+        drupalSettings.geolocation.commonMap,
+
+        /**
+         * @param {String} mapId - ID of current map
+         * @param {CommonMapSettings} commonMapSettings - settings for current map
+         */
+        function (mapId, commonMapSettings) {
+          if (
+            typeof commonMapSettings.dynamic_map !== 'undefined'
+            && commonMapSettings.dynamic_map.enable
+          ) {
+            var map = Drupal.geolocation.getMapById(mapId);
+
+            if (!map) {
+              return;
+            }
+
+            if (map.container.hasClass('geolocation-common-map-leaflet-processed')) {
+              return;
+            }
+            map.container.addClass('geolocation-common-map-leaflet-processed');
+
+            /**
+             * Update the view depending on dynamic map settings and capability.
+             *
+             * One of several states might occur now. Possible state depends on whether:
+             * - view using AJAX is enabled
+             * - map view is the containing (page) view or an attachment
+             * - the exposed form is present and contains the boundary filter
+             * - map settings are consistent
+             *
+             * Given these factors, map boundary changes can be handled in one of three ways:
+             * - trigger the views AJAX "RefreshView" command
+             * - trigger the exposed form causing a regular POST reload
+             * - fully reload the website
+             *
+             * These possibilities are ordered by UX preference.
+             */
+            if (
+              map.container.length
+              && map.type === 'leaflet'
+            ) {
+              map.addPopulatedCallback(function (map) {
+                var geolocationMapIdleTimer;
+                map.leafletMap.on('moveend zoomend', /** @param {LeafletMouseEvent} e */function (e) {
+                  clearTimeout(geolocationMapIdleTimer);
+
+                  geolocationMapIdleTimer = setTimeout(
+                    function () {
+                      var ajaxSettings = Drupal.geolocation.commonMap.dynamicMapViewsAjaxSettings(commonMapSettings);
+
+                      // Add bounds.
+                      var currentBounds = map.leafletMap.getBounds();
+                      var bound_parameters = {};
+                      bound_parameters[commonMapSettings['dynamic_map']['parameter_identifier'] + '[lat_north_east]'] = currentBounds.getNorthEast().lat;
+                      bound_parameters[commonMapSettings['dynamic_map']['parameter_identifier'] + '[lng_north_east]'] = currentBounds.getNorthEast().lng;
+                      bound_parameters[commonMapSettings['dynamic_map']['parameter_identifier'] + '[lat_south_west]'] = currentBounds.getSouthWest().lat;
+                      bound_parameters[commonMapSettings['dynamic_map']['parameter_identifier'] + '[lng_south_west]'] = currentBounds.getSouthWest().lng;
+
+                      ajaxSettings.submit = $.extend(
+                        ajaxSettings.submit,
+                        bound_parameters
+                      );
+
+                      Drupal.ajax(ajaxSettings).execute();
+                    },
+                    commonMapSettings.dynamic_map.views_refresh_delay
+                  );
+                });
+              });
+            }
+          }
+        });
+    },
+    detach: function (context, drupalSettings) {}
+  };
+
+})(jQuery, Drupal);
+;
